6be73becaf124a34b4fa7bc24659eea500e29f8a
Evelyne Maquelin <maqueline@catlin.edu>
2017-09-27 04:47:13 -0400

    eyyy2

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 	    background-color: white;
 	}
 </style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript">
 	$.ajax({
 		type: "POST",
