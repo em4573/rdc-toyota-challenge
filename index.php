7800025dfdab462cee2708dc182a4c53cb09cdfa
em4573 <maqueline@catlin.edu>
2017-09-29 21:06:39 +0000

    brython added

--- index.php
+++ index.php
@@ -15,10 +15,27 @@
 <script type="text/javascript"
     src="https://cdn.rawgit.com/brython-dev/brython/stable/www/src/brython.js">
 </script>
+<p style="visibility: hidden" id = "box">12,12,12</p>
+<body onload="brython()">
 <script type="text/python">
-	col = [24.5, 24.0, 23.5, 23.75, 23.5, 24.0, 24.0, 24.25, 24.0, 24.0, 23.75, 23.75, 23.5, 24.5, 24.75, 24.75, 24.25, 25.5, 24.75, 32.25, 25.5, 22.0, 26.25, 31.25, 24.5, 24.75, 28.25, 30.25, 30.25, 30.0, 30.25, 30.0, 23.75, 24.25, 26.25, 28.5, 30.25, 31.0, 30.75, 29.75, 23.75, 23.5, 24.0, 24.75, 25.25, 28.0, 29.25, 29.25, 23.5, 23.5, 23.75, 24.25, 24.5, 28.0, 29.5, 30.5, 23.5, 24.0, 23.5, 23.5, 23.75, 26.0, 28.75, 30.0]s
+from browser import document
+col = [24.5, 24.0, 23.5, 23.75, 23.5, 24.0, 24.0, 24.25, 24.0, 24.0, 23.75, 23.75, 23.5, 24.5, 24.75, 24.75, 24.25, 25.5, 24.75, 32.25, 25.5, 22.0, 26.25, 31.25, 24.5, 24.75, 28.25, 30.25, 30.25, 30.0, 30.25, 30.0, 23.75, 24.25, 26.25, 28.5, 30.25, 31.0, 30.75, 29.75, 23.75, 23.5, 24.0, 24.75, 25.25, 28.0, 29.25, 29.25, 23.5, 23.5, 23.75, 24.25, 24.5, 28.0, 29.5, 30.5, 23.5, 24.0, 23.5, 23.5, 23.75, 26.0, 28.75, 30.0]
+res = ""
+for i in range(len(col)):
+	res = res + str(col[i]) + ","
+
+document["box"].text = res
 </script>
 <script type="text/javascript">
+	var coltext = document.getElementById("box").innerHTML;
+	coltext = coltext.split(",");
+	var col = [];
+	for (i = 0; i < 64; i++) {
+		col.push(parseFloat(coltext[i]));
+	}
+	console.log(coltext);
+	console.log(col);
+
 	var tocol = function (num) {
 		var norm = (num - 15) / 28;
 	    var val = (norm * 255);
@@ -49,7 +66,7 @@
 	    
 	    row += '</div>';
 	    
-	    inner.append(row);
+	    inner += row;
 	}
 
 	document.getElementById('pixelgrid').innerHTML = inner;
